cdd9d1e4bedb6e7738ca907936b2115f8c75894c
otaruit <kimuranatsume5656@gmail.com>
2024-08-09 11:55:46 +0900

    gitignore対応

--- main.py
+++ main.py
@@ -1,7 +1,12 @@
-
-
+import json
 import jpy_datareader as jdr
-api_key = "105b557649ea929878fe7f927b520fcfa202ccee"
 
+
+# env.json ファイルを読み込む
+with open('env.json', 'r') as f:
+    env_vars = json.load(f)
+
+# API_KEY を取得する
+api_key = env_vars['API_KEY']
 df = jdr.get_data_estat_statsdata(api_key, statsDataId="0003000795")
 print(df)

b102362a3547f4dd59acd153e00d6831cb40b216
otaruit <kimuranatsume5656@gmail.com>
2024-08-13 21:46:52 +0900

    コメント削除

--- main.py
+++ main.py
@@ -2,34 +2,57 @@ import json
 import jpy_datareader as jdr
 import pandas as pd
 import streamlit as st
+import matplotlib.pyplot as plt
+import japanize_matplotlib # 追加
 
-# env.json ファイルを読み込む
+
+
 with open('env.json', 'r') as f:
     env_vars = json.load(f)
 
-# API_KEY を取得する
+
 api_key = env_vars['API_KEY']
 df = jdr.get_data_estat_statsdata(api_key, statsDataId="0003293502")
 
-
-# データフレームの先頭を表示
-# print(df)
+print(df)
 
 
-# データのフィルタリング
-df_filtered = df[(df['性別'] == '女子') &  (df['体格測定・テスト項目'] == 'テスト_10m障害物歩行（秒）')]
+df_filtered = df[
+    (df['性別'] == '女子') &
+    (df['体格測定・テスト項目'] == 'テスト_10m障害物歩行（秒）')
+]
 
-print(df_filtered)
 
-# # データの整形
-# df_grouped = df.groupby(["性別", "身長"])["出生数"].sum().reset_index()
+df_grouped = df_filtered.groupby(['運動部・スポーツクラブ所属', '時間軸(年度次)'])['平均値'].mean().unstack()
 
-# # 性別ごとにデータを分けて円グラフを表示
-# for gender in df_grouped["性別"].unique():
-#     df_gender = df_grouped[df_grouped["性別"] == gender]
-    
-#     st.subheader(f"{gender}の身長別出生数")
-#     st.write(df_gender)  # データを表示（オプション）
 
-#     # 円グラフの作成
-#     st.pyplot(df_gender.set_index("身長")["出生数"].plot(kind='pie', autopct='%1.1f%%', title=f"{gender}の身長別出生数").get_figure())
+st.title('運動部・スポーツクラブ所属別のテスト結果')
+
+
+fig, ax = plt.subplots(figsize=(12, 6))
+
+
+bar_width = 0.4
+
+years = df_grouped.columns
+
+r1 = range(len(years))
+r2 = [x + bar_width for x in r1]
+
+
+bars1 = ax.bar(r1, df_grouped.loc['所属していない'], color='skyblue', width=bar_width, edgecolor='grey', label='所属していない')
+bars2 = ax.bar(r2, df_grouped.loc['所属している'], color='salmon', width=bar_width, edgecolor='grey', label='所属している')
+
+
+ax.set_xlabel('年度')
+ax.set_ylabel('平均値')
+ax.set_title('テスト_10m障害物歩行（秒） 年度別: 所属している vs 所属していない')
+ax.set_xticks([x + bar_width / 2 for x in r1])
+ax.set_xticklabels(years)
+ax.legend()
+
+
+ax.set_ylim(6.5, 8)
+
+
+st.pyplot(fig)
